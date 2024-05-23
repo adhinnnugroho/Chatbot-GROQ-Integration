8183043b7bb15556bd5322f051d1e171c62ab902
adhinnnugroho <adhinnnugroho@gmail.com>
2024-05-23 10:23:25 +0700

    refactor: remove code not used

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,7 @@ function App() {
   return (
     <>
       <TypingEffect text={data} speed={80} />
-      {/* <div className="text-white">{data}</div> */}
-      <div className="max-w-2xl mx-auto mt-[35rem]">
+      <div className="max-w-2xl mx-auto lg:mt-[35rem] mt-[30rem]">
         <div className="flex items-center py-2 px-3 bg-gray-50 rounded-lg dark:bg-gray-700">
           <textarea id="content" rows="1" className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
           <button type="button" onClick={handleSubmit}
@@ -27,19 +26,7 @@ function App() {
           </button>
         </div>
       </div>
-      {/* <div>
-        <h1 classNameName='text-6xl font-bold text-white mt-5'>Chatbot with GROG</h1>
-        <div classNameName="">
-          <div classNameName="block">
-            <input type='text' name='' id='content' classNameName='mt-10 w-[30rem] rounded-lg py-2 px-4' />
-          </div>
-          <button onClick={handleSubmit} classNameName='bg-blue-600 p-2 text-white rounded-lg flex justify-center content-center mx-auto mt-5 w-[20rem]'>
-            Send
-          </button>
-        </div>
-
-        <div classNameName="text-white">{data}</div>
-      </div> */}
+  
     </>
   )
 }
